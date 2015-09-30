3d554c188feff2afb686bd16cb6540f8031e4aa5
Sanjith David <sdavid@deloitte.com>
2015-09-30 14:28:07 -0400

    HTML5 Application Cache for offline

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -62,3 +62,15 @@ function loadApp()
 {
 app.loadAppFlag=1;
 }
+
+function logEvent(event) {
+      console.log(event.type);
+  }
+ 
+  window.applicationCache.addEventListener('checking',logEvent,false);
+  window.applicationCache.addEventListener('noupdate',logEvent,false);
+  window.applicationCache.addEventListener('downloading',logEvent,false);
+  window.applicationCache.addEventListener('cached',logEvent,false);
+  window.applicationCache.addEventListener('updateready',logEvent,false);
+  window.applicationCache.addEventListener('obsolete',logEvent,false);
+  window.applicationCache.addEventListener('error',logEvent,false);

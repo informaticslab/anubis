4c4b581866335b0af0bf251094f875d33795ed22
sanjith <sanjith_david@yahoo.com>
2015-12-09 12:14:05 -0500

    Fixed issue where part1 page was not showing in IE11

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -17,7 +17,7 @@ var app=document.querySelector('#app');
 // }
 
 
-app.page = "aboutus";
+app.page = "part1";
 app.loadAppFlag=0;
 app.moremenu=["About this app","Help & FAQ","Contact"];
 
@@ -35,12 +35,12 @@ app.moremenu=["About this app","Help & FAQ","Contact"];
 // this.$.pages.selected = this.$.pages.selected === 2 ? 0 : (this.$.pages.selected + 1);
 // }
 
-window.addEventListener('WebComponentsReady', function() {
-// Explicitly call the register() method. We need to wait until the template's variables are
-// all set first, since the configuration depends on bound variables.
-document.querySelector('platinum-sw-register').register();
-console.log("registered");
-});
+// window.addEventListener('WebComponentsReady', function() {
+// // Explicitly call the register() method. We need to wait until the template's variables are
+// // all set first, since the configuration depends on bound variables.
+// document.querySelector('platinum-sw-register').register();
+// console.log("registered");
+// });
 
 
 app.displayInstalledToast = function() {

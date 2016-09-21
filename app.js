381ae7c05881e169e52997d32effd6ebe600a03c
Peter White <pwhitebe@gmail.com>
2016-09-21 17:26:19 -0400

    Updating to use properties file.

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var app = express();
 //var compression = require('compression');
 var path = require('path');
 var rootPath = path.normalize(__dirname + '/');
-
+var properties = require('./envProperties');
+httpsPort = 4400;
+httpPort = 4044;
 
 //app.use(compression());
 
@@ -13,31 +15,41 @@ app.set('view engine','jade');
 //app.set('bower_components', rootPath + '/public/bower_components');
 app.use(express.static(rootPath+'/public'));
 
-
-var server = app.listen(4044, function () {
+if(properties.USESSL == 'false')
+{
+var server = app.listen(httpPort, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('App listening at http://%s:%s', host, port);
 
 });
+}
+else if (properties.USESSL == 'true')
+{
+ var https = require('https'),      // module for https
+    fs =    require('fs');         // required to read certs and keys
 
+var options = {
+    key:    fs.readFileSync(properties.SSL_KEY),
+    cert:   fs.readFileSync(properties.SSL_CERT),
+    ca:     fs.readFileSync(properties.SSL_BUNDLE),
+    requestCert:        false,
+    rejectUnauthorized: false,
+};
 
+https.createServer(options, app).listen(httpsPort);
 
-// var https = require('https'),      // module for https
-//     fs =    require('fs');         // required to read certs and keys
+console.log('https app listening at '+ httpsPort);
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    //res.writeHead(301, { "Location": "https://localhost:4400" });
+    res.end();
+}).listen(httpPort);
 
-// var options = {
-//     key:    fs.readFileSync('/sec/certs/server-key.pem'),
-//     cert:   fs.readFileSync('/sec/certs/server-cert.pem'),
-//     ca:     [fs.readFileSync('/sec/certs/gd_bundle-g2.crt'),fs.readFileSync('/sec/certs/HHSPIVcachn.pem')],
-//     requestCert:        false,
-//     rejectUnauthorized: false,
-// };
-
-// https.createServer(options, app).listen(4400);
-
-
+console.log('http redirect listening at '+ httpPort);
+}
 app.get('/', function(req, res) {
   res.render('index');
   //res.render("views/index");
--- envProperties.js
+++ envProperties.js
@@ -5,4 +5,5 @@ module.exports = {
   SSL_CERT:  '/path/to/server_cert.pem',
   SSL_KEY:  '/path/to/server_key.pem',
   SSL_BUNDLE: '/path/to/gd_bundle-g2.crt',
+
 };
